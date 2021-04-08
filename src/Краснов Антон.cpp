4e1b9ff2adc6663280b0c9b187892af6c2250bd0
Anton <78906032+AntoniKR@users.noreply.github.com>
2021-04-09 01:46:47 +0300

    Update and rename alg.cpp to Краснов Антон.cpp

---
+++ src/Краснов Антон.cpp
@@ -0,0 +1,47 @@
+// Copyright 2021 NNTU-CS
+
+int countPairs1(int* arr, int len, int value)
+{
+    int chet = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if ((arr[i] + arr[j]) == value)
+                chet++;
+        }
+    }
+    return chet;
+}
+int countPairs2(int* arr, int len, int value)
+{
+    int chet = 0;
+    for (int i = 0; i < len - 1; i++)
+        for (int j = len - 1; j < len; j--)
+        {
+            if (i == j)
+                return chet;
+            else if ((arr[i] + arr[j]) == value)
+                chet++;
+        }
+    return chet;
+}
+
+int countPairs3(int* arr, int len, int value)
+{
+    int chet = 0;
+    int size = 0;
+    for (int z = 1; z < len; z++)
+    {
+        if ((arr[z]+arr[0]) == value)
+            size = z;
+    }
+
+    int twoElement = value - arr[0];
+    for (int i = 0; i <= size; i++)
+    {
+        if (arr[i] == twoElement)
+            chet++;
+    }
+    return chet;
+}
