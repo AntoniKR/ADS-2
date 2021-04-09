869a3fe1960d64365e331dfdf038d4a4e6768bcd
Anton <78906032+AntoniKR@users.noreply.github.com>
2021-04-09 14:58:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,30 +12,41 @@ int countPairs1(int* arr, int len, int value) {
 
 int countPairs2(int* arr, int len, int value) {
     int chet = 0;
-    int i = len - 1;
-    while (arr[i] > value) {
-        i--;
-    }
-    for (i; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            if ((arr[i] + arr[j]) == value)
-                chet++;
+    for (int i = len - 1; i > 0; i--) {
+        if (arr[i] <= value) {
+            for (int j = 0; j < i; j++) {
+                if ((arr[i] + arr[j]) == value)
+                    chet++;
+            }
         }
+        else
+            break;
     }
     return chet;
 }
 
 int countPairs3(int* arr, int len, int value) {
     int chet = 0;
-    int size = len - 1;
-    while (arr[size] > value) {
-        size--;
-    }
-    for (int i = 0; i < size; i++) {
-        int twoElement = value - arr[i];
-        for (int j = i + 1; j <= size; j++) {
-            if (arr[j] == twoElement)
-                chet++;
+    for (int i = 0; i < len - 1; i++) {
+        int left = i + 1, right = len, twoElement = value - arr[i];
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (arr[middle] == twoElement) {
+                while ((arr[middle - 1] == twoElement) && (middle > left)) {
+                    middle--;
+                }
+                while (arr[middle] == twoElement) {
+                    middle++;
+                    chet++;
+                }
+                break;
+            }
+            else if (arr[middle] > twoElement) {
+                right = middle;
+            }
+            else {
+                left = middle + 1;
+            }
         }
     }
     return chet;
