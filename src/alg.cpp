f04c2c0845b7df17d1931ea5724f6f659929b81f
Anton <78906032+AntoniKR@users.noreply.github.com>
2021-04-09 13:28:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,36 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
     int chet = 0;
-    for (int i = 0; i < len - 1; i++)
-        for (int j = i + 1; j < len; j++)
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
             if ((arr[i] + arr[j]) == value)
                 chet++;
+        }
+    }
     return chet;
 }
 
 int countPairs2(int* arr, int len, int value) {
     int chet = 0;
     int i = len - 1;
-    while (arr[i] > value)
+    while (arr[i] > value) {
         i--;
-    for (i; i > 0; i--)
+    }
+    for (i; i > 0; i--) {
         for (int j = 0; j < i; j++) {        
             if ((arr[i] + arr[j]) == value)
                 chet++;
         }
+    }
     return chet;
 }
 
 int countPairs3(int* arr, int len, int value) {
     int chet = 0;
     int size = len - 1;
-    while (arr[size] > value)
+    while (arr[size] > value) {
         size--; 
+    }
     for (int i = 0; i < size; i++) {
         int twoElement = value - arr[i];
         for (int j = i + 1; j <= size; j++) {
