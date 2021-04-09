65dd15d7e749069284bb4510f95b980956383b97
Anton <78906032+AntoniKR@users.noreply.github.com>
2021-04-09 13:09:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value){
     int chet = 0;
     for (int i = 0; i < len - 1; i++)
         for (int j = i + 1; j < len; j++)
@@ -10,15 +9,14 @@ int countPairs1(int* arr, int len, int value)
     return chet;
 }
 
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value){
     int chet = 0;
     int i = len - 1;
     while (arr[i] > value)
         i--;
 
     for (i; i > 0; i--)
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < i; j++){
          
             if ((arr[i] + arr[j]) == value)
                 chet++;
@@ -26,17 +24,16 @@ int countPairs2(int* arr, int len, int value)
     return chet;
 }
 
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value){
     int chet = 0;
 
     int size = len - 1;
     while (arr[size] > value)
         size--;
   
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++){
         int twoElement = value - arr[i];
-        for (int j = i + 1; j <= size; j++) {
+        for (int j = i + 1; j <= size; j++){
             if (arr[j] == twoElement)
                 chet++;
         }
